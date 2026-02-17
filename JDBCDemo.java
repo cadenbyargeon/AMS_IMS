b6c220bbed08dfd9a42e29ab7a64a22232399264
AsmiM <80651012+AsmithaMuthukumar@users.noreply.github.com>
2026-02-17 03:10:57 +0000

    made demo file

--- JDBCDemo.java
+++ JDBCDemo.java
@@ -1,6 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class JDBCDemo {
     public static void main(String[] args) {
-        
+        String url = "jdbc:mysql://localhost:3306/ALM-IMS-DB";
+        String user = "root";
+        String password = System.getenv("DB_PASSWORD");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         
     }
 }
