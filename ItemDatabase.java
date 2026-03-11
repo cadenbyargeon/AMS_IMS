8668824e178cea74a34ac7e347720e29ce48f39d
AsmiM <80651012+AsmithaMuthukumar@users.noreply.github.com>
2026-03-11 01:21:54 +0000

    added a user database class, edited itemdatabase

--- ItemDatabase.java
+++ ItemDatabase.java
@@ -12,14 +12,7 @@ public class ItemDatabase {
 
             Connection connection = DriverManager.getConnection(url, user, password);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from personInfo");
 
-            while(resultSet.next())
-            {
-                System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2)+" "+resultSet.getString(3)+" "+resultSet.getInt(4));
-
-
-            }
 
             connection.close();
         } 
@@ -27,6 +20,15 @@ public class ItemDatabase {
             System.out.println(e);
         }
         
+    } // end main
+
+    public void non_serialized_object_to_database(Non_Serialized n)
+    {
+        
+
+        
     }
+
+
 }
 
---
+++ UserDatabase.java
@@ -0,0 +1,55 @@
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+public class UserDatabase {
+    ArrayList<User> users;
+
+    public UserDatabase()
+    {
+        users = new ArrayList<User>();
+    }
+
+    public void addUser(User user)
+    {
+        if (user == null)
+        {
+            return;
+
+        }
+
+        if(findUser(user.getUsername()) != -1)
+        {
+            return;
+        }
+
+        users.add(user);
+    }
+
+    public int findUser(String username)
+    {
+        for (int i = 0; i < users.size(); i++)
+        {
+            User user = users.get(i);
+            if(username == user.getUsername())
+            {
+                return i;
+            }
+        }
+
+        return -1;
+
+    }
+
+    public User findUserByIndex(int index)
+    {
+        if (index < 0 || index >= users.size())
+        {
+            return null;
+        }
+        else 
+        {
+            return users.get(index);
+        }
+    }
+
+    
+}
