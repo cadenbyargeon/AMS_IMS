1133d17a06d0dfc9f03dba7bbe410264da154e0a
AsmiM <80651012+AsmithaMuthukumar@users.noreply.github.com>
2026-03-30 15:26:29 +0000

    testing of database connection

--- ItemDatabase.java
+++ ItemDatabase.java
@@ -1,26 +1,34 @@
+import java.io.File;
 import java.sql.*;
+//import java.io.File;
+
 
 public class ItemDatabase {
 
     public static void main(String[] args) {
+
+        //File f = new File("/workspaces/my-java-project/ca.pem");
+        // System.out.println("Exists? " + f.exists());
+        String path = new File("ca.pem").getAbsolutePath();
+
         String url = "jdbc:mysql://ams-ims-db-amsims.i.aivencloud.com:24455/ams-ims-db"
            + "?sslMode=VERIFY_CA"
-           + "&sslCa=/workspaces/AMS_IMS/ca.pem";
+           + "&sslCa=" + path;
+
+        /*String url = "jdbc:mysql://ams-ims-db-amsims.i.aivencloud.com:24455/ams-ims-db"
+           + "?sslMode=VERIFY_CA"
+           + "&sslCa=/workspaces/AMS_IMS/ca.pem";*/
         String user = "avnadmin";
         String password = "AVNS_gYcciwH3FiHQ6J6WzGi";
-
-        try {
+        
+       try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-
-            Connection connection = DriverManager.getConnection(url, user, password);
-                        System.out.println("Connected successfully!");
-
-
-
-            connection.close();
-        } 
-        catch (Exception e) {
-            System.out.println(e);
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected successfully!");
+            conn.close();
+        } catch (Exception e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
         }
         
     } // end main
