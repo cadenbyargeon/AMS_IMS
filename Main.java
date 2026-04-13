f05bafd2cb5f763f0b9758100d66d91c54e12289
AsmiM <80651012+AsmithaMuthukumar@users.noreply.github.com>
2026-04-13 19:09:59 +0000

    changed view so it displays item numbers

--- Main.java
+++ Main.java
@@ -923,8 +923,9 @@ public static void view(Scanner scan, ItemDatabase itemDB){
             System.out.println("Item not found.");
         } 
         else{
-            for (String item : foundItems) {
-                System.out.println(item);
+            for (int i = 0; i < foundItems.size(); i++) {
+                System.out.println(i);
+                System.out.println(foundItems.get(i));
                 System.out.println("***************************");
             }
 
